f97ad19ed694652195084857ffbaabb973b41889
uchida <uchida.atsushi.8@gmail.com>
2014-02-22 00:50:24 +0900

    add index.ts

---
+++ index.ts
@@ -0,0 +1,10 @@
+declare var ace: any;
+
+$(() => {
+    var Leditor = ace.edit("editor-left");
+    Leditor.setTheme("ace/theme/xcode");
+    Leditor.getSession().setMode("ace/mode/javascript");
+    var Reditor = ace.edit("editor-right");
+    Reditor.setTheme("ace/theme/xcode");
+    Reditor.getSession().setMode("ace/mode/javascript");
+});
